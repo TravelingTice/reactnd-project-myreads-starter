8121a7906bafc8fdd98c00923bd5e2b5e227d163
Matthijs Kralt <matthijs29@live.com>
2018-03-06 18:55:13 +0100

    feat: Add escape-string-regexp and sort-by as dependencies to the project

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 class Search extends React.Component {
+  state = {
+    query: '',
+    books: []
+  }
+
+  search = (query) => {
+    this.setState({ query: query.trim() })
+  }
   render() {
     return (
         <div className="search-books">
           <div className="search-books-bar">
-            <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+            <Link className="close-search" to="/">Close</Link>
             <div className="search-books-input-wrapper">
               {/*
                 NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -16,7 +25,11 @@ class Search extends React.Component {
                 However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                 you don't find a specific author or title. Every search is limited by search terms.
               */}
-              <input type="text" placeholder="Search by title or author"/>
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value={this.state.query}
+                onChange={(e) => this.search(e.target.value)}/>
 
             </div>
           </div>
