908efc448447d45094c4ff4a326e59c7ecf588a3
Matthijs Kralt <matthijs29@live.com>
2018-03-06 18:17:19 +0100

    feat: Install react-router-dom, move Search screen to Search component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 // Import components
 import Book from './Book'
+import Search from './Search'
 
 class BooksApp extends React.Component {
   state = {
     // An array of books currently in our library
     books: [],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
   }
 
   // Change shelf property of book to value of shelf ("wantToRead", "currentlyReading", "read")
@@ -36,28 +30,6 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -107,10 +79,9 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <a to="">Add a book</a>
             </div>
           </div>
-        )}
       </div>
     )
   }
--- src/Book.js
+++ src/Book.js
@@ -8,7 +8,8 @@ class Book extends React.Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
             <div className="book-shelf-changer">
-              <select onChange={e => this.props.onChangeShelf(this.props.book, e.target.value)}>
+            {/* Controlled component: When value of select changes -> the shelf of the book gets updated and dom re-rendered*/}
+              <select value={this.props.book.shelf} onChange={e => this.props.onChangeShelf(this.props.book, e.target.value)}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
